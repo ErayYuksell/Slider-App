e1f671b364bf5c4e9d99571a62feb63e1a317eab
Eray Yüksel <yukseleray203@gmail.com>
2022-07-15 15:36:23 +0300

    Rastgele Slider yapıldı ancak click olayı yinede çalışmıyor

--- script.js
+++ script.js
@@ -1,54 +1,113 @@
 var models = [
     {
         name : 'Bmw 418d',
-        image : 'image/bmw.jpg',
+        image : 'img/bmw.jpg',
         link : 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'
     },
     {
         name : 'Mazda CX-3',
-        image : 'image/mazda.jpg',
+        image : 'img/mazda.jpg',
         link : 'http://www.arabalar.com.tr/mazda/cx-3/2017/1-5-sky-d-motion'
     },
     {
         name : 'Volvo S60',
-        image : 'image/volvo.jpg',
+        image : 'img/volvo.jpg',
         link : 'http://www.arabalar.com.tr/volvo/s60/2018/1-5-t3-advance'
     },
     {
         name : 'Skoda Superb',
-        image : 'image/skoda.jpg',
+        image : 'img/skoda.jpg',
         link : 'http://www.arabalar.com.tr/skoda/superb/2018/1-4-tsi-active'
     },
     {
         name : 'Honda Civic',
-        image : 'image/honda.jpg',
+        image : 'img/honda.jpg',
         link : 'http://www.arabalar.com.tr/honda/civic/2018/1-6-elegance'
     }
 ];
 
-var index=3;
-var slaytcount=models.length;
-showslide(index);
-document.querySelector('.fa-solid fa-circle-arrow-left fa-2x').addEventListener('click',function(){
+var index = 0;
+var slaytCount = models.length;
+var interval;
+
+var settings={
+    duration : '1000',
+    random : false
+};
+
+init(settings);
+
+document.querySelector('.fa-solid fa-circle-arrow-left fa-2x arrow').addEventListener('click',function(){
     index--;
-    showslide(index);
+    showSlide(index);
     console.log(index);
 });
-document.querySelector('.fa-solid fa-circle-arrow-right fa-2x').addEventListener('click',function(){
-    index++;
-    showslide(index);
 
+document.querySelector('.fa-solid fa-circle-arrow-right fa-2x arrow').addEventListener('click',function(){
+    index++;
+    showSlide(index);
+    console.log(index);    
 });
-function showslide(i){
-    index=i;
-    if(i<0){
-        index=slaytcount-1;
-    }
-    if(i>=slaytcount){
-        index=0;
-    }
-    document.querySelector('.card-title').textContent=models[index].name;
-    document.querySelector('.card-img-top').setAttribute('src',models[index].image);
-    document.querySelector('.card-link').setAttribute('href',models[index].link);
-    
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval);
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings);
+    })
+})
+
+
+function init(settings){
+
+    var prev;
+    interval=setInterval(function(){
+        
+        if(settings.random){
+            // random index
+            do{
+                index = Math.floor(Math.random() * slaytCount);
+            }while(index == prev)
+            prev = index;
+        }else{
+            // artan index
+            if(slaytCount == index+1){
+                index = -1;
+            }
+            showSlide(index);
+            console.log(index);
+            index++;
+        }
+        showSlide(index);
+
+    },settings.duration)
+  
+
 }
+
+
+
+function showSlide(i){
+
+    index = i;
+
+    if (i<0) {
+        index = slaytCount - 1;
+    }
+    if(i >= slaytCount){
+        index =0;
+    }
+
+    document.querySelector('.card-title').textContent = models[index].name;
+
+    document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+    
+    document.querySelector('.card-link').setAttribute('href',models[index].link);
+}
+
+
+
